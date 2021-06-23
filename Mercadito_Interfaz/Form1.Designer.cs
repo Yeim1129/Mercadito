c1d93631917d6a18db0247a97ca5ac5a9ce113bd
J Manuel Hernandez <jmanuelhaa@gmail.com>
2021-06-23 01:22:46 -0500

    Mercadito_Proyecto final

---
+++ Mercadito_Interfaz/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace Mercadito_Interfaz
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.contraseña = new Mercadito3_0.txtAutoexplicativo();
+            this.usuario = new Mercadito3_0.txtAutoexplicativo();
+            this.botonAcceder = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contraseña
+            // 
+            this.contraseña.BackColor = System.Drawing.Color.Gainsboro;
+            this.contraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.contraseña.ColorTextoVacio = System.Drawing.Color.Gray;
+            this.contraseña.Font = new System.Drawing.Font("Consolas", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contraseña.Location = new System.Drawing.Point(317, 172);
+            this.contraseña.Name = "contraseña";
+            this.contraseña.PasswordChar = '*';
+            this.contraseña.Size = new System.Drawing.Size(250, 21);
+            this.contraseña.TabIndex = 0;
+            this.contraseña.TextoVacio = "Contraseña";
+            this.contraseña.UseSystemPasswordChar = true;
+            // 
+            // usuario
+            // 
+            this.usuario.BackColor = System.Drawing.Color.Gainsboro;
+            this.usuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.usuario.ColorTextoVacio = System.Drawing.Color.DimGray;
+            this.usuario.Font = new System.Drawing.Font("Consolas", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usuario.Location = new System.Drawing.Point(317, 139);
+            this.usuario.Name = "usuario";
+            this.usuario.Size = new System.Drawing.Size(250, 21);
+            this.usuario.TabIndex = 0;
+            this.usuario.TextoVacio = "Usuario";
+            // 
+            // botonAcceder
+            // 
+            this.botonAcceder.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.botonAcceder.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.botonAcceder.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.botonAcceder.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.botonAcceder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonAcceder.Location = new System.Drawing.Point(344, 258);
+            this.botonAcceder.Name = "botonAcceder";
+            this.botonAcceder.Size = new System.Drawing.Size(200, 23);
+            this.botonAcceder.TabIndex = 5;
+            this.botonAcceder.Text = "Acceder";
+            this.botonAcceder.UseVisualStyleBackColor = false;
+            this.botonAcceder.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Firebrick;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(235, 350);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("GeoSlab703 Md BT", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 34);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "MERCADITO";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Mercadito_Interfaz.Properties.Resources.WhatsApp_Image_2021_03_13_at_2_04_58_PM;
+            this.pictureBox2.Location = new System.Drawing.Point(59, 172);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(106, 92);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::Mercadito_Interfaz.Properties.Resources.toldo2_adobespark;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(235, 125);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Mercadito_Interfaz.Properties.Resources.window_minimize;
+            this.pictureBox4.Location = new System.Drawing.Point(592, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Mercadito_Interfaz.Properties.Resources.window_close;
+            this.pictureBox3.Location = new System.Drawing.Point(625, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(372, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 32);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "ACCEDER";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.RosyBrown;
+            this.linkLabel1.Location = new System.Drawing.Point(435, 196);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(132, 13);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "¿Ha olvidado contraseña?";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2.LinkColor = System.Drawing.Color.RosyBrown;
+            this.linkLabel2.Location = new System.Drawing.Point(417, 294);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(70, 15);
+            this.linkLabel2.TabIndex = 9;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Registrarse";
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BurlyWood;
+            this.ClientSize = new System.Drawing.Size(650, 350);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.botonAcceder);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.usuario);
+            this.Controls.Add(this.contraseña);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Inicio";
+            this.Opacity = 0.93D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inicio";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Mercadito3_0.txtAutoexplicativo contraseña;
+        private Mercadito3_0.txtAutoexplicativo usuario;
+        private System.Windows.Forms.Button botonAcceder;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
---
+++ Mercadito_Interfaz/Form2.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Mercadito_Interfaz
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAutoexplicativo1 = new Mercadito3_0.txtAutoexplicativo();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAutoexplicativo2 = new Mercadito3_0.txtAutoexplicativo();
+            this.txtAutoexplicativo3 = new Mercadito3_0.txtAutoexplicativo();
+            this.txtAutoexplicativo4 = new Mercadito3_0.txtAutoexplicativo();
+            this.txtAutoexplicativo5 = new Mercadito3_0.txtAutoexplicativo();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(114, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "REGISTRARSE";
+            // 
+            // txtAutoexplicativo1
+            // 
+            this.txtAutoexplicativo1.ColorTextoVacio = System.Drawing.Color.Gray;
+            this.txtAutoexplicativo1.Location = new System.Drawing.Point(25, 96);
+            this.txtAutoexplicativo1.Name = "txtAutoexplicativo1";
+            this.txtAutoexplicativo1.Size = new System.Drawing.Size(250, 20);
+            this.txtAutoexplicativo1.TabIndex = 2;
+            this.txtAutoexplicativo1.TextoVacio = "Nombre";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackgroundImage = global::Mercadito_Interfaz.Properties.Resources._66c535a2d4ab89a138dd31dcf5d08167;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(334, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 311);
+            this.panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Image = global::Mercadito_Interfaz.Properties.Resources.pngfind_com_thomas_the_dank_engine_6738833_1_;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(30, 152);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(232, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Acceder con Google";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Image = global::Mercadito_Interfaz.Properties.Resources.pngfind_com_thomas_the_dank_engine_6738833_1_;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(30, 96);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Acceder con Facebook";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Location = new System.Drawing.Point(89, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "INICIAR CON";
+            // 
+            // txtAutoexplicativo2
+            // 
+            this.txtAutoexplicativo2.ColorTextoVacio = System.Drawing.Color.Gray;
+            this.txtAutoexplicativo2.Location = new System.Drawing.Point(25, 122);
+            this.txtAutoexplicativo2.Name = "txtAutoexplicativo2";
+            this.txtAutoexplicativo2.Size = new System.Drawing.Size(250, 20);
+            this.txtAutoexplicativo2.TabIndex = 2;
+            this.txtAutoexplicativo2.TextoVacio = "Apellidos";
+            // 
+            // txtAutoexplicativo3
+            // 
+            this.txtAutoexplicativo3.ColorTextoVacio = System.Drawing.Color.Gray;
+            this.txtAutoexplicativo3.Location = new System.Drawing.Point(25, 152);
+            this.txtAutoexplicativo3.Name = "txtAutoexplicativo3";
+            this.txtAutoexplicativo3.Size = new System.Drawing.Size(250, 20);
+            this.txtAutoexplicativo3.TabIndex = 2;
+            this.txtAutoexplicativo3.TextoVacio = "Direccion";
+            // 
+            // txtAutoexplicativo4
+            // 
+            this.txtAutoexplicativo4.ColorTextoVacio = System.Drawing.Color.Gray;
+            this.txtAutoexplicativo4.Location = new System.Drawing.Point(25, 178);
+            this.txtAutoexplicativo4.Name = "txtAutoexplicativo4";
+            this.txtAutoexplicativo4.Size = new System.Drawing.Size(250, 20);
+            this.txtAutoexplicativo4.TabIndex = 2;
+            this.txtAutoexplicativo4.TextoVacio = "Nombre";
+            // 
+            // txtAutoexplicativo5
+            // 
+            this.txtAutoexplicativo5.ColorTextoVacio = System.Drawing.Color.Gray;
+            this.txtAutoexplicativo5.Location = new System.Drawing.Point(25, 195);
+            this.txtAutoexplicativo5.Name = "txtAutoexplicativo5";
+            this.txtAutoexplicativo5.Size = new System.Drawing.Size(250, 20);
+            this.txtAutoexplicativo5.TabIndex = 2;
+            this.txtAutoexplicativo5.TextoVacio = "Nombre";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(634, 311);
+            this.Controls.Add(this.txtAutoexplicativo5);
+            this.Controls.Add(this.txtAutoexplicativo4);
+            this.Controls.Add(this.txtAutoexplicativo3);
+            this.Controls.Add(this.txtAutoexplicativo2);
+            this.Controls.Add(this.txtAutoexplicativo1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private Mercadito3_0.txtAutoexplicativo txtAutoexplicativo1;
+        private System.Windows.Forms.Button button2;
+        private Mercadito3_0.txtAutoexplicativo txtAutoexplicativo2;
+        private Mercadito3_0.txtAutoexplicativo txtAutoexplicativo3;
+        private Mercadito3_0.txtAutoexplicativo txtAutoexplicativo4;
+        private Mercadito3_0.txtAutoexplicativo txtAutoexplicativo5;
+    }
+}
